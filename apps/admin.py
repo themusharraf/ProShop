4f29b6fbdb00cfcab08d1a5e43962f6a409b2dd6
musharrafz <meibrohimov@gmail.com>
2023-05-08 22:29:23 +0500

    add admin register

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from apps.models import Category, Product, ProductImage, Tags
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.Admin):
+    ...
+
+
+@admin.register(Product)
+class ProductAdmin(admin.Admin):
+    ...
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.Admin):
+    ...
+
+
+@admin.register(Tags)
+class TagsAdmin(admin.Admin):
+    ...
